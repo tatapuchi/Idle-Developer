777eb2d96748f2fb3027d9d88e524e9f32325db8
Vibhor Singh <vibdaddy12@gmail.com>
2021-02-22 19:08:19 +0100

    Added Player Class

---
+++ Idle.Core/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Idle.Core.Models
+{
+    public class Player
+    {
+        // XP Cap to advance to the next Level
+        public int Cap;
+
+        //Experience Points
+        public int XP { get; private set; }
+
+        //Level
+        public int Level { get; private set; }
+
+        //Amount of In-Game Currenncy
+        public int Coins { get; private set; }
+
+
+    }
+}
--- Idle/Idle/Helpers/FileHelper.cs
+++ Idle/Idle/Helpers/FileHelper.cs
@@ -1,34 +1,42 @@
-﻿using System;
+﻿using Idle.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
 namespace Idle.Helpers
 {
-    //Helper class that handles file IO for data persistence
+    //Non-instantiable Helper class that handles file IO for data persistence
     public class FileHelper
     {
+
+        //Non-instantiable Helper Class
+        private FileHelper()
+        {
+            throw new InvalidOperationException();
+        }
+
+
         //File Path to json file containing Player data
         private static readonly string file = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Player.json");
 
 
         //Serializes Player object and writes the content to Player.json
-        public static void WritePlayer(object obj)
+        public static void WritePlayer(Player player)
         {
-            string json = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(player);
             File.WriteAllText(file, json);
         }
 
 
         //Reads Player.json and returns a deserialized Player object from the file data
-        public static object ReadPlayer()
+        public static Player ReadPlayer()
         {
             string json = File.ReadAllText(file);
-            object obj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            Player player = (Player) Newtonsoft.Json.JsonConvert.DeserializeObject(json);
 
-            return obj;
+            return player;
         }
-
 
     }
 }
