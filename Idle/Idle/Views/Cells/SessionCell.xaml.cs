be8e6f944809a247ca887dcab3991f13d65ba049
Vibhor Singh <vibdaddy12@gmail.com>
2021-03-05 19:41:58 +0100

    BindingContext bug

--- Idle/Idle/Views/Cells/SessionCell.xaml.cs
+++ Idle/Idle/Views/Cells/SessionCell.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Idle.Core.Models;
+using Idle.Core.Models.Fields;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,25 +14,36 @@ namespace Idle.Views.Cells
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SessionCell : ViewCell
     {
-
-
+        //Perhaps set the Task.Delay() value to be bound to the level of the itemsource object with some multiplier
+        private int _level;
+        private int _xp;
+        private Languages _language;
 
         public SessionCell()
         {
             InitializeComponent();
+            //_level = Int32.Parse(LevelLabel.Text);
+            //_xp = Int32.Parse(XPLabel.Text);
+
             Task.Run(async () =>
             {
 
+                
                 while (true)
                 {
                     Device.BeginInvokeOnMainThread(() =>
                     {
                         if (ProgressBar.Progress >= 1.0f)
                         {
-                            int Level = Int32.Parse(LevelLabel.Text); Level++;
-                            int XP = Int32.Parse(XPLabel.Text); XP += 35;
-                            LevelLabel.Text = Level.ToString();
-                            XPLabel.Text = XP.ToString();
+                            //_level++;
+                            //_xp++;
+                            //var level = Int32.Parse(LevelLabel.Text);
+                            //var xp = Int32.Parse(XPLabel.Text);
+                            //LevelLabel.Text = level.ToString();
+                            //XPLabel.Text = xp.ToString();
+                            _language.Level++;
+                            _language.XP += 35;
+                            BindingContext = _language;
 
                             ProgressBar.Progress = 0.0f;
                         }
@@ -42,15 +55,20 @@ namespace Idle.Views.Cells
                     await Task.Delay(40);
 
                 }
+
             });
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _language = BindingContext as Languages;
+
         }
 
         private void Upgrade_Clicked(object sender, EventArgs e)
         {
-            if (ProgressBar.Progress <= 0.9f)
-            {
-                ProgressBar.Progress += 0.1f;
-            }
+            if (ProgressBar.Progress <= 0.9f) { ProgressBar.Progress += 0.1f; }
             else { ProgressBar.Progress = 1.0f; }
         }
     }
