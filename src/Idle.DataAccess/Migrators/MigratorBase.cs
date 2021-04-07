3fccb2d79ff4f9acaf3a918f981b9a42011b23ee
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2021-04-07 22:29:59 +0200

    added migrators

---
+++ src/Idle.DataAccess/Migrators/MigratorBase.cs
@@ -0,0 +1,36 @@
+﻿using Idle.DataAccess.Common;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Idle.DataAccess.Migrators
+{
+	public abstract class MigratorBase<TModel>
+		where TModel : ModelBase
+	{
+		private static SQLiteConnection _connection;
+
+		public MigratorBase()
+		{
+			_connection = new SQLiteConnection(Constants.DatabasePath);
+		}
+
+		public void Migrate()
+		{
+			if (DoesTableExist())
+				return;
+
+			_connection.CreateTable<TModel>();
+		}
+
+		protected abstract string TableName { get; }
+
+		private bool DoesTableExist()
+		{
+			var table = _connection.Execute($"SELECT name FROM sqlite_master WHERE type = 'table' AND name = '{TableName}'");
+			if (table == 0) return false;
+			return true;
+		}
+	}
+}
