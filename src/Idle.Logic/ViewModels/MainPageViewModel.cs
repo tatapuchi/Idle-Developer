921dd77755928d8a224b484cf4056fadc68f94f7
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2021-05-20 19:49:32 +0200

    implemented mainpage vm

--- src/Idle.Logic/ViewModels/MainPageViewModel.cs
+++ src/Idle.Logic/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using Idle.DataAccess.Repositories;
+using Idle.Logic.Common;
+using Idle.Logic.Interfaces;
+using Idle.Logic.Languages;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Idle.Logic.ViewModels
 {
 	class MainPageViewModel : ViewModelBase
 	{
+
+		private readonly INavigationService _navigation;
+		private readonly LanguageRepository _languageRepository;
+
+		public MainPageViewModel(INavigationService navigation, LanguageRepository languageRepository)
+			: this()
+		{
+			_navigation = navigation;
+			_languageRepository = languageRepository;
+		}
+
+		private MainPageViewModel()
+		{
+			NavigateToLanguagesPageCommand = new Command(async _ => await _navigation.PushAsync<LanguagesViewModel>(true));
+		}
+
+		public ICommand NavigateToLanguagesPageCommand { get; }
+
 	}
 }
