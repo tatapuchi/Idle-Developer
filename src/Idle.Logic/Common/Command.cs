de820a921c1f43907f8d202f138c498fa08d0a32
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2021-05-20 19:44:30 +0200

    added mainpageVm and Command classes

---
+++ src/Idle.Logic/Common/Command.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Idle.Logic.Common
+{
+	class Command : Command<object>
+	{
+
+		public Command(Action<object> execute) : base(execute){ }
+
+		public Command(Action<object> execute, Predicate<object> canExecute) 
+			: base(execute, canExecute) { }
+	}
+
+	class Command<TParameter> : ICommand
+	{
+		public event EventHandler CanExecuteChanged;
+
+		private readonly Action<TParameter> _execute;
+		private readonly Predicate<TParameter> _canExecute;
+
+		public Command(Action<TParameter> execute)
+		{
+			_execute = execute;
+			_canExecute = (_) => true;
+		}
+
+		public Command(Action<TParameter> execute, Predicate<TParameter> canExecute)
+		{
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+
+		public bool CanExecute(object parameter) =>
+			_canExecute.Invoke((TParameter)parameter);
+
+		public void Execute(object parameter)
+		{
+			if (!CanExecute((TParameter)parameter)) return;
+			_execute.Invoke((TParameter)parameter);
+		}
+	}
+}
