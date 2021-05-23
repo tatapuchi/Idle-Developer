c28c63ea8a84a4d17f70a60ce260afc22561b109
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2021-05-23 20:25:30 +0200

    cleanup, SetProperty returns true if proprty changed, using expression bodied members for properties when possible

--- src/Idle.Logic/Languages/LanguageViewModel.cs
+++ src/Idle.Logic/Languages/LanguageViewModel.cs
@@ -18,17 +18,21 @@ namespace Idle.Logic.Languages
     public class LanguageViewModel : ViewModelBase, IImage
     {
         internal readonly Language _language;
-        public Command<LanguageViewModel> XPCommand { get; set; }
 
         public LanguageViewModel(Language language) 
             : this()
         {
             _language = language;
 
-            ImagePath = language.ImagePath;
             Name = language.Name;
             Description = language.Description;
             Difficulty = language.Difficulty;
+            ImagePath = language.ImagePath;
+            
+            XP = language.XP;
+            Level = language.Level;
+            Grade = language.Grade;
+
             XPCost = language.XPCost;
             XPIncome = language.XPIncome;
         }
@@ -39,17 +43,9 @@ namespace Idle.Logic.Languages
         }
 
         
-
 		public ICommand GainProgressCommand { get; }
 
-        #region Model-props
-
-        public void UpdateXP<T>(T obj)
-        {
-            if (typeof(T) != typeof(LanguageViewModel)) { throw new ArgumentException("Command Parameter must be of type LanguageViewModel"); }
-            var viewModel = obj as LanguageViewModel;
-            viewModel.GainProgress();
-        }
+        #region Props
 
         public string ImagePath { get; }
         public string Name { get; }
@@ -58,78 +54,75 @@ namespace Idle.Logic.Languages
         public int XPCost { get; }
         public int XPIncome { get; }
 
-        #endregion Model-props
-
-        #region Props
-
         //Setting default value even though the value will be set in the constructor
         private int _xp = 0;
-        public int XP 
-        { 
-            get { return _xp; } 
-            private set 
-            {
-                _xp = value;
+        public int XP
+		{
+			get => _xp;
+			private set
+			{
+				if (!TrySetProperty(ref _xp, value)) return;
 
-            //This part checks if the XP is high enough to increment the Level, this is needed in here as when we load data in from the db
-            //we want to provide the language objects with only the XP, and then let the setters handle the updating of the Level and Grade
-                while (_xp > LevellingAmount())
-                {
+				var lvlingAmount = LevellingAmount();
 
-                    _xp -= LevellingAmount();
-                    Level++;
-                }
+				//This part checks if the XP is high enough to increment the Level, this is needed in here as when we load data in from the db
+				//we want to provide the language objects with only the XP, and then let the setters handle the updating of the Level and Grade
+				while (_xp > lvlingAmount)
+				{
+					_xp -= lvlingAmount;
+					Level++;
+				}
+			}
+		}
 
-                OnPropertyChanged(nameof(XP));
-            } 
-        }
+		//Setting default value even though the value will be set in the constructor
+		private int _level = 1;
+        public int Level
+		{
+			get => _level;
+			private set
+			{
+				if (!TrySetProperty(ref _level, value)) return;
 
-        //Setting default value even though the value will be set in the constructor
-        private int _level = 1;
-        public int Level { 
-            get { return _level; } 
-            private set 
-            {
-                _level = value;
-                //SetProperty(ref _level, value);
+				//I prefer to use guard clause over if-else because its a lot easier to read this way and more concise
+				if (value > 5) { Grade = "D"; }
+				if (value > 10) { Grade = "C"; }
+				if (value > 20) { Grade = "B"; }
+				if (value > 30) { Grade = "A"; }
+				if (value > 40) { Grade = "S"; }
+				if (value > 50) { Grade = "S+"; }
+				if (value > 100) { Grade = "S++"; }
+			}
+		}
 
-                //I prefer to use guard clause over if-else because its a lot easier to read this way and more concise
-                if (value > 5) { Grade = "D"; }
-                if (value > 10) { Grade = "C"; }
-                if (value > 20) { Grade = "B"; }
-                if (value > 30) { Grade = "A"; }
-                if (value > 40) { Grade = "S"; }
-                if (value > 50) { Grade = "S+"; }
-                if (value > 100) { Grade = "S++"; }
-            } 
-        }
+		private string _grade = "F";
+        public string Grade
+		{
+			get => _grade;
+            set => TrySetProperty(ref _grade, value);
+		}
 
-        private string _grade = "F";
-        public string Grade 
-        {
-            get { return _grade; }
-            set { _grade = value; OnPropertyChanged(nameof(Grade)); }
-            //private set => SetProperty(ref _grade, value); 
-        }
+		// The progress of our progress bar in the view
+		private float _progress = 0.0f;
+        public float Progress
+		{
+			get => _progress;
+			set
+			{
+				if (!TrySetProperty(ref _progress, value)) return;
+				if (_progress >= 1f)
+				{
+					_progress = 0f;
+					GainXP();
+				}
+			}
+		}
 
-        // The progress of our progress bar in the view
-        private float _progress = 0.0f;
-        public float Progress 
-        { 
-            get { return _progress; } 
-            set 
-            {
-                _progress = value;
-                if (_progress >= 1f) { _progress = 0f; GainXP(); }
-                OnPropertyChanged(nameof(Progress));
-            } 
-        }
-
-        #endregion Props
+		#endregion Props
 
 
-        //Adds to XP based on the XPIncome property of the concretion
-        public void GainXP()
+		//Adds to XP based on the XPIncome property of the concretion
+		public void GainXP()
         {
             XP += XPIncome;
         }
