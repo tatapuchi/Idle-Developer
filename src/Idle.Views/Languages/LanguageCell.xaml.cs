7ecf42e21ca810bf4e407f2c720702f903a1207f
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2021-05-26 20:20:21 +0200

    when progress is saved the progress timer animation has to be adjusted for the first load

--- src/Idle.Views/Languages/LanguageCell.xaml.cs
+++ src/Idle.Views/Languages/LanguageCell.xaml.cs
@@ -29,7 +29,7 @@ namespace Idle.Views.Languages
 			set => BindingContext = value; 
 		}
 
-		protected override void OnBindingContextChanged()
+		protected async override void OnBindingContextChanged()
 		{
 			base.OnBindingContextChanged();
 
@@ -38,21 +38,26 @@ namespace Idle.Views.Languages
 				progressBar.AbortAnimation(_progressbarAnimationName);
 				return;
 			}
-			
-			// this method call is intended not to be avaited
-			ProgressTo(_progressbarAnimationName, 1, _duration, Easing.Linear, true);
+			await OnBindingContextChangedImpAsync();
+		}
+
+		private async Task OnBindingContextChangedImpAsync()
+		{
+			// the progress is saved in the db. this means we have to calculate the remaining duration.
+			// after the progress bar loaded once. we need to use the infinite animation till we press a button
+			var remainingDuration = CalculateRemainingDuration();
+			await ProgressTo(_progressbarAnimationName, 1, remainingDuration, Easing.Linear, false);
+			_ = ProgressTo(_progressbarAnimationName, 1, _duration, Easing.Linear, true); // this task runs infinite. we dont await it
 		}
 
 		private async void GainProgressClicked(object sender, EventArgs e)
 		{
 			// we have to stop the animation before we encrease the progress with a button. otherwise the progress will not change via a button
 			progressBar.AbortAnimation(_progressbarAnimationName);
-			ViewModel.GainProgressCommand.Execute(null); 
-			
+			ViewModel.GainProgressCommand.Execute(null);
+
 			// when we encrease the progress with a button and resume the animation we have to calculate how fast the resuming animation has to be
-			double remainingProgress = 1 - ViewModel.Progress;
-			double speed = 1 / (double)_duration;
-            uint remainingDuration = (uint)(remainingProgress / speed);
+			var remainingDuration = CalculateRemainingDuration();
 
 			await ProgressTo(_progressbarAnimationName, 1, remainingDuration, Easing.Linear, false);
 			progressBar.AbortAnimation(_progressbarAnimationName);
@@ -85,6 +90,15 @@ namespace Idle.Views.Languages
 			}
 
 			return tcs.Task;
+		}
+
+		private uint CalculateRemainingDuration()
+		{
+			double remainingProgress = 1 - ViewModel.Progress;
+			double speed = 1 / (double)_duration;
+			uint remainingDuration = (uint)(remainingProgress / speed);
+
+			return remainingDuration;
 		}
 	}
 }
