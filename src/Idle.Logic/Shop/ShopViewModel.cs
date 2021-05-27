fcceb346ed64cf6a4c31bf2447fbe97ddfacecb3
Vibhor Singh <vibdaddy12@gmail.com>
2021-05-27 17:48:13 +0200

    Update ShopViewModel.cs

--- src/Idle.Logic/Shop/ShopViewModel.cs
+++ src/Idle.Logic/Shop/ShopViewModel.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace Idle.Logic.Shop
 {
+    //This is the viewmodel for the flyoutpage itself, this serves no other purpose but to be able to navigate to it
     public class ShopViewModel: ViewModelBase
     {
     }
