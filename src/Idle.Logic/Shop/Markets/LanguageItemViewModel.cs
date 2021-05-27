7d92a8aadac2f42f611837cdaefa553fd4c312a8
Vibhor Singh <vibdaddy12@gmail.com>
2021-05-27 19:12:36 +0200

    some changes

---
+++ src/Idle.Logic/Shop/Markets/LanguageItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Idle.Models.Fields;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Idle.Logic.Shop.Markets
+{
+    public class LanguageItemViewModel: ViewModelBase
+    {
+        internal readonly Language _language;
+
+        public LanguageItemViewModel(Language language)
+        {
+            this._language = language;
+        }
+
+        public string Name => _language.Name;
+        public string Description => _language.Description;
+
+        public int XPCost => _language.XPCost;
+    }
+}
---
+++ src/Idle.Logic/Shop/Markets/LanguageMarketViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Idle.Logic.Languages;
+using Idle.Models.Fields;
+using Idle.Models.Fields.Languages;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Idle.Logic.Shop.Markets
+{
+    public class LanguageMarketViewModel : ViewModelBase
+    {
+
+        public ObservableCollection<LanguageViewModel> Items { get; set; } = new ObservableCollection<LanguageViewModel>();
+
+        public LanguageMarketViewModel()
+        {
+            Items.Add(new LanguageViewModel(new CSharp()));
+            Items.Add(new LanguageViewModel(new Java()));
+            Items.Add(new LanguageViewModel(new Kotlin()));
+        }
+    }
+}
