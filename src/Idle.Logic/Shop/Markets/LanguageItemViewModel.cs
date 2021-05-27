0eee344f8a89e39ead30287dd21209e145b3061a
Vibhor Singh <vibdaddy12@gmail.com>
2021-05-27 20:05:49 +0200

    added purchaselanguage command

--- src/Idle.Logic/Shop/Markets/LanguageItemViewModel.cs
+++ src/Idle.Logic/Shop/Markets/LanguageItemViewModel.cs
@@ -1,7 +1,9 @@
-﻿using Idle.Models.Fields;
+﻿using Idle.Logic.Common;
+using Idle.Models.Fields;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Idle.Logic.Shop.Markets
 {
@@ -12,16 +14,17 @@ namespace Idle.Logic.Shop.Markets
         public LanguageItemViewModel(Language language)
         {
             this._language = language;
+            PurchaseLanguageCommand = new Command(_ => _language.Active = true); ;
         }
+
+        public ICommand PurchaseLanguageCommand { get; }
 
         public string Name => _language.Name;
         public string Description => _language.Description;
         public int XPCost => _language.XPCost;
         public string ImagePath => _language.ImagePath;
+         
+        // make sure langauge is removed from observablecollection when its bought
 
-        public void PurchaseLanguage()
-        {
-            _language.Active = true;
-        }
     }
 }
