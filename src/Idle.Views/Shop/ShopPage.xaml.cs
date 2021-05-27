f416bf99193e0e49916abbc4d02be121af854171
Vibhor Singh <vibdaddy12@gmail.com>
2021-05-27 18:26:21 +0200

    Update ShopPage.xaml.cs

--- src/Idle.Views/Shop/ShopPage.xaml.cs
+++ src/Idle.Views/Shop/ShopPage.xaml.cs
@@ -15,6 +15,27 @@ namespace Idle.Views.Shop
         public ShopPage()
         {
             InitializeComponent();
+
+            flyoutPage.listView.ItemSelected += OnItemSelected;
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                FlyoutLayoutBehavior = FlyoutLayoutBehavior.Popover;
+            }
+
         }
+
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as FlyoutPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                flyoutPage.listView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+
     }
 }
