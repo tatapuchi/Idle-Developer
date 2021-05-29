ca521127a0e23ed4c2b286142de5a21f2891d248
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2021-05-30 00:12:54 +0200

    added test project

---
+++ src/Idle.Resources/L10N/LocalizationService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Idle.Resources.L10N
+{
+	internal class LocalizationResourceWrapper : LocalizationResource
+	{
+		private static string[] _possibleCultures { get; } = CultureInfo.GetCultures(CultureTypes.AllCultures)
+				.Where(c => string.IsNullOrWhiteSpace(c.TwoLetterISOLanguageName))
+				.Select(c => c.TwoLetterISOLanguageName)
+				.ToArray();
+
+		//protected string[] GetDefinedCultures()
+		//{
+		//	var path = typeof(LocalizationResource).Assembly.
+		//}
+
+	}
+
+
+	public class LocalizationService
+	{
+		private static CultureInfo _usedCulture = new CultureInfo("en");
+		
+		
+
+
+		public void SetCulture(string cultureName)
+		{
+			var culture = new CultureInfo(cultureName);
+
+			
+
+		}
+
+		//public string GetString(string key, )
+		//{
+		//	var result = LocalizationResource.ResourceManager.
+		//}
+	}
+}
---
+++ src/Tests/Idle.Resources.Tests/L10N/LocalizationResourceWrapperTests.cs
@@ -0,0 +1,15 @@
+using Idle.Resources.L10N;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Idle.Resources.Tests.L10N
+{
+	[TestClass]
+	public class LocalizationResourceWrapperTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var resource = new LocalizationResourceWrapper();
+		}
+	}
+}
