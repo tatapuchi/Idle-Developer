be061433ef42dc728bf2d0c4b618db36032acc18
Vibhor Singh <vibdaddy12@gmail.com>
2021-05-29 11:21:55 +0200

    added fadetriggeraction, contained in /Triggers

---
+++ src/Idle.Views/Triggers/FadeTriggerAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Idle.Views.Triggers
+{
+    public class FadeTriggerAction : TriggerAction<VisualElement>
+    {
+
+        protected override void Invoke(VisualElement sender)
+        {
+            sender.FadeTo(0.4, 1000);
+        }
+    }
+}
