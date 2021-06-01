ea00a34dfe947c9bf52211166b6fba5000d1bb2c
Vibhor Singh <vibdaddy12@gmail.com>
2021-06-01 19:37:47 +0200

    IPermaBuff interface and a concretion

---
+++ src/Idle.DataAccess/ModelTemplates/Buffs/TestBuff.cs
@@ -0,0 +1,15 @@
+﻿using Idle.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Idle.DataAccess.ModelTemplates.Buffs
+{
+    public class TestBuff : IPermaBuff
+    {
+        public string Name => "Test Buff";
+        public float PlayerXPMultiplierAddon { get; set; } = 0f;
+        public float XPMultiplierAddon { get; set; } = 0.8f;
+        public float SpeedMultiplierAddon { get; set; } = 0f;
+    }
+}
---
+++ src/Idle.Models/Interfaces/IPermaBuff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Idle.Models.Interfaces
+{
+    public interface IPermaBuff
+    {
+        public string Name { get; }
+        public float PlayerXPMultiplierAddon { get; set; }
+        public float XPMultiplierAddon { get; set; }
+        public float SpeedMultiplierAddon { get; set; }
+    }
+}
