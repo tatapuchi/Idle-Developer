11f7127e05d1c3db31495069458c96c0f7d0ef34
Vibhor Singh <vibdaddy12@gmail.com>
2021-06-01 18:43:35 +0200

    setting buffs up

---
+++ src/Idle.Models/Interfaces/IBuffing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Idle.Models.Interfaces
+{
+    public interface IBuffing
+    {
+
+        public float XPMultiplier { get; set; }
+        public float SpeedMultiplier { get; set; }
+
+    }
+}
