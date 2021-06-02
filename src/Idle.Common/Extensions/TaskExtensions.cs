c9ea56dc5209b2e176b2633249476185a5abf38e
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2021-06-02 23:07:59 +0200

    added async command, common assembly and references, logging missing, asynccommand not used

---
+++ src/Idle.Common/Extensions/TaskExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idle.Common.Extensions
+{
+	public static class TaskExtensions
+	{
+		public static async void AwaitAsync(this Task task)
+		{
+			try
+			{
+				await AwaitAsyncImplementation(task);
+			}
+			catch (Exception e)
+			{
+				// todo: add logging
+				throw;
+			}
+		}
+
+		private static async Task AwaitAsyncImplementation(Task task) => await task;
+	}
+}
---
+++ src/Idle.Logic/Common/AsyncCommand.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Idle.Common.Extensions;
+
+namespace Idle.Logic.Common
+{
+    public interface IAsyncCommand : ICommand
+    {
+        Task ExecuteAsync();
+        bool CanExecute();
+    }
+
+    public interface IAsyncCommand<T> : ICommand
+    {
+        Task ExecuteAsync(T parameter);
+        bool CanExecute(T parameter);
+    }
+
+    public class AsyncCommand : IAsyncCommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private bool _isExecuting;
+        private readonly Func<Task> _execute;
+        private readonly Func<bool> _canExecute;
+
+        public AsyncCommand(
+            Func<Task> execute,
+            Func<bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+		public bool CanExecute() => !_isExecuting && (_canExecute?.Invoke() ?? true);
+
+		public async Task ExecuteAsync()
+        {
+            if (CanExecute())
+            {
+                try
+                {
+                    _isExecuting = true;
+                    await _execute();
+                }
+                finally
+                {
+                    _isExecuting = false;
+                }
+            }
+
+            RaiseCanExecuteChanged();
+        }
+
+		public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+		bool ICommand.CanExecute(object parameter) => CanExecute();
+		void ICommand.Execute(object parameter) => ExecuteAsync().AwaitAsync();
+		
+	}
+
+   
+    public class AsyncCommand<T> : IAsyncCommand<T>
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private bool _isExecuting;
+        private readonly Func<T, Task> _execute;
+        private readonly Func<T, bool> _canExecute;
+
+        public AsyncCommand(Func<T, Task> execute, Func<T, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+		public bool CanExecute(T parameter) => !_isExecuting && (_canExecute?.Invoke(parameter) ?? true);
+
+		public async Task ExecuteAsync(T parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                try
+                {
+                    _isExecuting = true;
+                    await _execute(parameter);
+                }
+                finally
+                {
+                    _isExecuting = false;
+                }
+            }
+
+            RaiseCanExecuteChanged();
+        }
+
+		public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+		bool ICommand.CanExecute(object parameter) => CanExecute((T)parameter);
+		void ICommand.Execute(object parameter) => ExecuteAsync((T)parameter).AwaitAsync();
+		
+	}
+
+}
