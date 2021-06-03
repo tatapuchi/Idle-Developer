0d076f75f8912f23f7a6c970a71b9a8386805b6f
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2021-06-03 15:13:58 +0200

    added logger interface, strategies, main logger and todos

---
+++ src/Idle.Common/Diagnostics/ILogger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Idle.Common.Diagnostics
+{
+
+	public interface ILogger
+	{
+		void Log(LogMessage logMessage);
+	}
+
+
+	internal class EmptyLoggerStrategy : ILogger
+	{
+		public void Log(LogMessage logMessage) { }
+	}
+
+	internal class ConsoleLoggerStrategy : ILogger
+	{
+		public void Log(LogMessage logMessage) => Debug.WriteLine(logMessage.ToString());
+	}
+
+	// todo: https://www.msctek.com/xamarin-forms-logging-with-appcenter/
+	// we will probably have to use a multi targeting project.
+	// we will do this before deploying the app.
+	// we would only have to implement the strategy and not change any other code
+	//internal class AppCenetLoggerStrategy : ILogger
+	//{
+	//	public void Log(LogMessage logMessage)
+	//	{
+	//		throw new System.NotImplementedException();
+	//	}
+	//}
+
+	public class Logger : ILogger
+	{
+		// facade pattern. we dont want to expose the strategies in other assemblies
+		// thats why the strategies are internal
+		// DI is not required because the logger used in another assembly will not be tested.
+		// mocking is enabled because of coupling to an interface
+		private readonly static IEnumerable<ILogger> _loggers = new List<ILogger>()
+		{
+			new EmptyLoggerStrategy(),
+			new ConsoleLoggerStrategy()
+		};
+
+		public void Log(LogMessage logMessage)
+		{
+			foreach (var logger in _loggers)
+				logger.Log(logMessage);
+		}
+	}
+
+	
+}
+
+
---
+++ src/Idle.Common/Diagnostics/LogMessage.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+#nullable enable
+
+namespace Idle.Common.Diagnostics
+{
+	public class LogMessage
+	{
+		private const string _logPrefix = "### ";
+
+		public LogMessage(
+			[CallerMemberName] string callerName = null, 
+			[CallerFilePath] string filePath = null, 
+			[CallerLineNumber] int? lineNumber = null)
+		{
+			CallerName = callerName;
+			FilePath = filePath;
+			LineNumber = lineNumber;
+		}
+
+		public LogMessage(
+			Exception exception,
+			[CallerMemberName] string callerName = null,
+			[CallerFilePath] string filePath = null,
+			[CallerLineNumber] int? lineNumber = null) 
+		: this(callerName, filePath, lineNumber)
+		{
+			Exception = exception;
+		}
+
+		public LogMessage(
+			string message,
+			[CallerMemberName] string callerName = null,
+			[CallerFilePath] string filePath = null,
+			[CallerLineNumber] int? lineNumber = null)
+		: this(callerName, filePath, lineNumber)
+		{
+			Message = message;
+		}
+
+		public LogMessage(
+			Exception exception,
+			string message,
+			[CallerMemberName] string callerName = null,
+			[CallerFilePath] string filePath = null,
+			[CallerLineNumber] int? lineNumber = null)
+		: this(callerName, filePath, lineNumber)
+		{
+			Exception = exception;
+			Message = message;
+		}
+
+		// diagnostics
+		public string? CallerName { get; }
+		public string? FilePath { get; }
+		public int? LineNumber { get; }
+
+		public string? Message { get; }
+		public Exception? Exception { get; set; }
+
+		public override string ToString()
+		{
+			const string message = nameof(Message);
+			const string exception = nameof(Exception);
+			const string callerName = nameof(CallerName);
+			const string filePath = nameof(FilePath);
+			const string lineNumber = nameof(LineNumber);
+
+			var sb = new StringBuilder(_logPrefix);
+			AppendIfNotNull(sb, message, Message);
+			AppendIfNotNull(sb, exception, Exception);
+			AppendIfNotNull(sb, callerName, CallerName);
+			AppendIfNotNull(sb, filePath, FilePath);
+			AppendIfNotNull(sb, lineNumber, LineNumber);
+
+			return sb.ToString();
+		}
+
+		private static void AppendIfNotNull(StringBuilder sb, string prefix, object? value)
+		{
+			if (value != null)
+				sb.Append(prefix).Append(value).Append(Environment.NewLine);
+		}
+	}
+}
+
+#nullable disable
